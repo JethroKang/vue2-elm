7015d82bf72d4548646793dc6b89d6a107fb564a
JethroKang <1421444203@qq.com>
2017-09-05 20:57:13 +0800

    update

--- src/page/confirmOrder/confirmOrder.vue
+++ src/page/confirmOrder/confirmOrder.vue
@@ -179,7 +179,6 @@
         mounted(){
 
 
-
             //判断当前用户是否登录
             if (!(this.userInfo && this.userInfo.token)) {
                 this.showAlert = true;
@@ -269,14 +268,32 @@
             //发送订单信息
             Request.Post('order', {token:this.userInfo.token,address_id:this.choosedAddress.id,orders:[{goods_id: this.foodID,sku_spec_id: this.foodIndex,num: this.numCounter}],content:this.remarklist})
             .then((res) => {
-              this.orderRes=res;
+              this.orderRes=res.data.jsapi;
               //下单成功，跳转支付
-              if(this.orderRes.code === 200){
-                this.$router.push('/confirmOrder/payment');
-              }else{
-                this.showAlert = true;
-                this.alertText = this.orderRes.msg;
-              }
+//              if(this.orderRes.code === 200){
+//                this.$router.push('/confirmOrder/payment');
+//              }else{
+//                this.showAlert = true;
+//                this.alertText = this.orderRes.msg;
+//              }
+              console.log(this.orderRes);
+
+              wx.chooseWXPay({
+                timestamp: this.orderRes.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                nonceStr: this.orderRes.nonceStr, // 支付签名随机串，不长于 32 位
+                package: this.orderRes.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                signType: this.orderRes.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                paySign: this.orderRes.paySign, // 支付签名
+                success: function (res) {
+                  // 支付成功后的回调函数
+                  alert("sssss");
+                  this.$router.push('/confirmOrder/payment');
+
+                }
+              });
+
+
+
             })
 
           }
